ba25422acfba1a5a6c7bfb2119c749cba57ad8f6
oslo2015 <475920112@qq.com>
2015-03-03 08:34:35 +0800

    Create sockConstants.h

---
+++ sockConstants.h
@@ -0,0 +1,19 @@
+
+#define serv_PORT           1987
+
+#define LEN_message         128
+
+#define host_REGISTER       1
+#define host_LOGOUT         2
+
+#define flow_START          11
+#define flow_OVER           12
+#define reduce_OVER         13
+
+#define app_CREATE          21
+#define app_OVER            22
+
+#define attempt_CREATE      31
+
+
+#define COMMUNICATION_THREAD    0
